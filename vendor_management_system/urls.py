cc65b536747e7b2dd739623f2d89e696a7d695ea
avishek376 <avishekdeb376@gmail.com>
2024-05-03 17:06:02 +0530

    initial commit

--- vendor_management_system/urls.py
+++ vendor_management_system/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/', include('vendor_app.urls')),
-    # path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
